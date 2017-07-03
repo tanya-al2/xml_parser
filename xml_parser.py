cb0d1d3971c08a19acfb51b95ceeb67b05b6df33
tanya-al2 <tanya-al2@yandex.ru>
2017-07-03 07:21:20 +0300

    add update button

--- xml_parser.py
+++ xml_parser.py
@@ -7,6 +7,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from re import *
 
+
 class Trey(QWidget):
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
@@ -23,19 +24,19 @@ class Trey(QWidget):
         tray_menu.addAction(close_action)
         tray_menu.addAction(activate_action)
         self.close_action_from_menu = False
-        tray_icon = QSystemTrayIcon(QIcon(u'xml_parser.ico'), self)
-        tray_icon.show()
-        tray_icon.setContextMenu(tray_menu)
-        tray_icon.activated.connect(self.activate_window)
-        tray_icon.showMessage(u'ошибочка', u'просто говорю, что')
+        self.tray_icon = QSystemTrayIcon(QIcon(u'xml_parser.ico'), self)
+        self.tray_icon.show()
+        self.tray_icon.setContextMenu(tray_menu)
+        self.tray_icon.activated.connect(self.activate_window)
+        # self.tray_icon.showMessage(u'ошибочка', u'просто говорю, что')
         # Создаем виджет с вкладкой для заметы содержания вкладок
         # главное окно
         self.main_tab_layout = QVBoxLayout(self)
         self.main_tab_widget = QTabWidget(self)
 
         # добавляем вкладки
-        self.main_tab_widget.addTab(ChangeXmlTab(), u'Окно для замены текста в тагах')
-        self.main_tab_widget.addTab(SettingTab(), u'Установка настроеек для утилиты автоподписи')
+        self.main_tab_widget.addTab(ChangeXmlTab(self), u'Окно для замены текста в тагах')
+        self.main_tab_widget.addTab(SettingTab(self), u'Установка настроеек для утилиты автоподписи')
 
         self.main_tab_layout.addWidget(self.main_tab_widget)
         self.setLayout(self.main_tab_layout)
@@ -50,9 +51,6 @@ class Trey(QWidget):
             self.tray_icon.showMessage('TypeError', u'невозможно найти ' + search_parameters)
         except AttributeError:
             self.tray_icon.showMessage('AttributeError', u'невозможно найти ' + search_parameters)
-
-    def setConfigVariables(self):
-        self.configVariables.clear()
 
     # Функции для работы с треем
     def close_from_menu(self):
@@ -69,29 +67,30 @@ class Trey(QWidget):
     def activate_window(self):
         self.show()
 
+
 # окно в котором считываются настройки для указанной в кофигах утилиты
 class SettingTab(Trey):
-    def __init__(self, parent=None):
+    def __init__(self, parent):
         QWidget.__init__(self, parent)
         # Добавляю виджеты
         # Сами виджеты
+        self._parent = parent
         self.main_windows_layout = QFormLayout()
-        self.tray_icon.showMessage('hy','i love you')
         # Добавляю кнопку для установки параметров в утилите автоподписи
         self.setup_config = QPushButton(u'Установить параметры в утилите')
         self.main_windows_layout.addRow(self.setup_config)
         self.update_config = QPushButton(u'Установить')
         self.main_windows_layout.addRow(self.update_config)
         self.setup_config.clicked.connect(self.read_and_past_configfile, True)
-        self.update_config.clicked.connect(self.updater)
+        self.update_config.clicked.connect(self.utility_config_updater)
         # Считываю из конфиг-файла поля и добавляю комбобоксы в окно для установки конфигов другой утилиты
-        self.read_installer()
+        self.to_change_parameters_config_reader()
         self.setLayout(self.main_windows_layout)
 
-    def read_installer(self):
+    def to_change_parameters_config_reader(self):
         config_variables = {}
 
-        for variable in self.search_pattern('(.*)=(.*)',
+        for variable in self._parent.search_pattern('(.*)=(.*)',
                                             read_file(os.path.join(os.curdir, 'xml_parser.conf')),
                                             u'параметры'):  # найти в конфигах
             key = variable[0]
@@ -100,25 +99,26 @@ class SettingTab(Trey):
             config_variables[key].addItems(variable[1].split())
             self.main_windows_layout.addRow(key, config_variables[key])
 
-    def updater (self):
+    def utility_config_updater(self):
         for i in range(self.main_windows_layout.rowCount(), 2, -1):
             self.main_windows_layout.removeRow(i)
-        self.read_installer()
+        self.to_change_parameters_config_reader()
         self.update()
 
-#, 'pathToSignMessage', self.config_variables['pathToSignMessage']
+    # , 'pathToSignMessage', self.config_variables['pathToSignMessage']
     def read_and_past_configfile(self, pastconfig=False):
         path_to_config = os.path.join(self.config_variables['pathToPutMessage'].currentText(), 'conf.properties')
         configs = read_file(path_to_config)
 
         if pastconfig:
             for key in self.config_variables:
-                configs = configs.replace(self.searchPattern('{0}=.*'.format(key), configs, key, True, 0),
+                configs = configs.replace(self._parent.search_pattern('{0}=.*'.format(key), configs, key, True, 0),
                                           '{0}='.format(key) + self.config_variables[key].currentText(), key)
             write_file(path_to_config, configs)
 
+
 class ChangeXmlTab(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent):
         QWidget.__init__(self, parent)
         # окно с которого все началось
         # Добавляю чек-боксы к окну
@@ -220,7 +220,6 @@ class ChangeXmlTab(QWidget):
             subprocess.Popen(
                 os.path.join(self.configVariables['pathToPutMessage'].currentText(), 'run.sh')).wait()  # .communicate()
 
-
     def change_tag_contant(self):
         list_of_files_for_change = os.listdir(os.path.join(os.curdir, 'in'))
         tag_or_text = {'tag': 1, 'text': 0}
@@ -273,7 +272,6 @@ class ChangeXmlTab(QWidget):
                             if not self.show_question_message():
                                 return
 
-
     def show_question_message(self):
         if not self.isHidden():
             self.show()
@@ -281,6 +279,7 @@ class ChangeXmlTab(QWidget):
         return True if QMessageBox.question(self, 'Message', u'Продолжить подмену текста и отправку файла на сервер?',
                                             QMessageBox.Yes | QMessageBox.No,
                                             QMessageBox.No) == QMessageBox.Yes else False
+
 
 def check_folder_exist(directory):
     if not os.path.exists(directory):
